098c5124235d534dcbd0cbfed8f7e1556333e1cc
Yurve <103430477+Yurve@users.noreply.github.com>
2022-08-01 00:22:43 +0900

    Update check_bt.py

--- check_bt.py
+++ check_bt.py
@@ -1,6 +1,10 @@
 import os
 from tabnanny import check
 
+
+#piscan을 자동화하기 위해 
+os.system("sudo hciconfig hci0 piscan")
+
 porcess_read = os.popen("ps -ef | grep bt_servo2.py | grep -v 'grep' ").readlines()
 # ps -ef 명령어를 이용해서 현재 프로세스를 출력한 후, 그 중 run24h.py 문자열이 포함된 줄만 모은다.
 # grep 명령어 자체도 프로세스에 나타나므로 grep -v를 이용해서 제외한다.
